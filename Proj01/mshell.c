686c36e2eff6c1785476ccdbb162153052a750bd
Yihang Xu <ccxuy@mothra.cs.albany.edu>
2013-09-11 15:30:27 -0400

    Folk run program success.

---
+++ Proj01/mshell.c
@@ -0,0 +1,63 @@
+/* $begin shellmain */
+#include "csapp.h"
+#define MAXARGS   128
+
+/* function prototypes */
+void eval(char *cmdline);
+
+int main() 
+{
+    char cmdline[MAXLINE]; /* Command line */
+
+    while (1) {
+        /* Read */
+        system("clear");
+        printf("\n");                   
+        printf("=======START========\n");                   
+        printf("Welcome to My Shell!\n");                   
+        printf("There is several options avaliable.\n");    
+        printf("1. program 1.\n");                   
+        printf("========END=========\n");                   
+        Fgets(cmdline, MAXLINE, stdin); 
+        if (feof(stdin))
+            exit(0);
+
+        /* Evaluate */
+        eval(cmdline);
+    } 
+}
+/* $end shellmain */
+  
+/* $begin eval */
+/* eval - Evaluate a command line */
+void eval(char *cmdline) 
+{
+    char *argv[MAXARGS]; /* Argument list execve() */
+    char buf[MAXLINE];   /* Holds modified command line */
+    int bg;              /* Should the job run in bg or fg? */
+    pid_t pid;           /* Process id */
+   
+    printf("cmd: %s",cmdline); 
+    printf(",%d\n",strcmp(cmdline, "quit"));
+    if (!strcmp(cmdline, "quit\n")) /* quit command */
+    {
+        exit(0);
+    }
+    
+    if (!strcmp(cmdline, "1\n"))    /* Run program 1*/
+    {
+        printf("cmd 1 received!\n");
+        
+        pid = Fork(); //line:ecf:forkreturn
+        if (pid == 0) {  /* Child */
+            execve("prog1", NULL, environ);
+        }
+    }
+
+    int status;
+    waitpid(pid, &status, 0);
+    //unix_error("waitfg: waitpid error");
+
+    return;
+}
+/* $end eval */
---
+++ Proj01/prog1.c
@@ -0,0 +1,33 @@
+#include "csapp.h"
+
+int main(int argc, char *arg[])
+{
+  char *pToInputLine = NULL;
+  /*This MUST be initialized, otherwise getline might
+    think the memory pointed to by garbage is good to
+    use or re-allocate with realloc!*/
+  int bufsize=0;
+  /*We MUST use a non-misleading, and even better, informative
+    name for the bufsize variable. My original "nread" was WRONG*/
+  int nret;
+  printf("Hello. A greeding from program 1.\n");
+  nret = getline( &pToInputLine, &bufsize, stdin);
+  printf("Thanks for line %s lengths: bufsize=%d nret=%d,", pToInputLine, bufsize, nret);
+  printf("\nGive me another command:");
+  nret = getline( & pToInputLine, &bufsize, stdin );
+  printf("Thanks for line %s Lengths: bufsize=%d nret=%d,", pToInputLine, bufsize, nret); 
+
+
+
+  if( strcmp(pToInputLine, "FirstCommand\n")==0)
+    {
+      printf("Thanks for that FirstCommand!\n");
+    }
+  else if( strcmp(pToInputLine, "Fork\n") == 0 )
+    {
+      int fret;
+      printf("hey man, I see you really want to try a Fork. Here goes\n");
+      fret = Fork();
+      printf("Fork() returned %d\n", fret);
+    }
+}
